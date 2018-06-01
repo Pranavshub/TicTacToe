e58e6d21c54bb5c3e70e89e715b80f60413a5661
elisabeth-the-llama <39343552+elisabeth-the-llama@users.noreply.github.com>
2018-06-01 11:23:51 -0700

    Add files via upload

--- Grid.java
+++ Grid.java
@@ -1,98 +1,109 @@
 
 public class Grid {
-	   public int [][] board;
-	   public Grid (int [][] board){
-		   //creates a multidimensional array representing the board
-		   //where 0 is blank, 1 is x, and 2 is o.
-	        this.board= board;
-	    }
-	 
+	public int[][] board;
 
-	public int win() {
-		//horizontal/vertical win checking code
-		
-		   //checks if there is a winning horizontal or vertical pattern and returns who won (1 or 2), or if it was a 
-		//draw (returns -1) or if there is no win or no draw (0)
-		   int horiz;
-		   for (int row=0; row<board.length; row++) {
-			   horiz=0; //resets to 0 for each new row
-			   for (int col=0; col<board[row].length; col++) {
-				   if (board[row][0]==board[row][col]&&board[row][0]!=0) {
-					   horiz++;
-				   }
-				   if (horiz==3) {
-					 if (board[row][0]==1) {
-						 return 1;
-					 }
-					 
-					 else {
-						 return 2;
-					 }
-					 //if the first number(which represents either an x or o, matches
-					 //with the rest of the numbers in the row (aka if the entire row has
-					 //the same number), return the number which won using horizontal pattern
-				   }
-				   
-				   if (row==0) {//goes through this only with the first row
-					   if (board[0][col]==board[1][col] && board[0][col]==board[2][col]&&board[0][col]!=0) {
-						   if (board[0][col]==1) {
-							   return 1;
-						   }
-						   else {
-							   return 2;
-						   }
-						   //if a number in the first row is the same as the two below it,
-						   //return the number/ x or o that won with a vertical pattern
-					   }
-				   }
-			   }
-		   }
-		   
-		   //diagonal win checking code
-		   
-		 //if the middle number equals the top right and bottom left numbers or equals the 
-		   //top left and bottom right numbers, return true to show a winning diagonal
-		   //pattern
-		   int mid= board[1][1];
-		   if (mid== board [0][0] && mid== board [2][2]&&mid!=0) { //top left and bottom right
-			   if (mid==1) {
-				   return 1;
-			   }
-			   else {
-				   return 2;
-			   }
-		   }
-		   else if (mid==board[0][2]&& mid==board[2][0]&&mid!=0) { //top right and bottom left
-			   if (mid==1) {
-				   return 1;
-			   }
-			   else {
-				   return 2;
-			   }
-		   }
-		   
-		   //draw checking code
-		   
-		   //checks if there are no remaining spaces but no win (a draw) and returns -1 if so
-		   int zeros=9;
-			for (int []arr:board) {
-				for (int a: arr) {
-					if (a==0) {
-						break;
+	public Grid(int[][] board) {
+		//creates a multidimensional array representing the board
+		// where 0 is blank, 1 is x, and 2 is o.
+		this.board = board;
+	}
+
+	public boolean win(int [][] b) { //code commented out can be put back in if the code needs to be modified to return
+		//who won
+		// horizontal/vertical win checking code
+
+		// checks if there is a winning horizontal or vertical pattern and returns who
+		// won (1 or 2), or if it was a
+		// draw (returns -1) or if there is no win or no draw (0)
+		int horiz;
+		for (int row = 0; row < b.length; row++) {
+			horiz = 0; // resets to 0 for each new row
+			for (int col = 0; col < b[row].length; col++) {
+				if (b[row][0] == b[row][col] && b[row][0] != 0) {
+					horiz++;
+				}
+				if (horiz == 3) {
+					return true;
+					/*if (b[row][0] == 1) {
+						return 1;
 					}
+
 					else {
-						zeros--;
+						return 2;
+					}
+					*/
+					// if the first number(which represents either an x or o, matches
+					// with the rest of the numbers in the row (aka if the entire row has
+					// the same number), return the number which won using horizontal pattern
+				}
+
+				if (row == 0) {// goes through this only with the first row
+					
+					if (b[0][col] == b[1][col] && b[0][col] == b[2][col] && b[0][col] != 0) {
+						return true;
+						/*if (b[0][col] == 1) {
+							return 1;
+						} else {
+							return 2;
+						}
+						*/
+						// if a number in the first row is the same as the two below it,
+						// return the number/ x or o that won with a vertical pattern
 					}
 				}
 			}
-			
-			if (zeros==0) {
-				return -1;
+		}
+
+		// diagonal win checking code
+
+		// if the middle number equals the top right and bottom left numbers or equals
+		// the
+		// top left and bottom right numbers, return true to show a winning diagonal
+		// pattern
+		int mid = b[1][1];
+		if (mid == b[0][0] && mid == b[2][2] && mid != 0) { // top left and bottom right
+			return true;
+			/*if (mid == 1) {
+				return 1;
+			} else {
+				return 2;
 			}
-			
-			//no win code
-		   return 0;//if there are no winning patterns found, return 0
-		   }
-	   
-	   }
-	   
+			*/
+		} else if (mid == b[0][2] && mid == b[2][0] && mid != 0) { // top right and bottom left
+			return true;
+			/*if (mid == 1) {
+				return 1;
+			} else {
+				return 2;
+			}
+			*/
+		}
+
+		// draw checking code
+
+		// checks if there are no remaining spaces but no win (a draw) and returns -1 if
+		// so
+		int zeros = 9;
+		for (int row= 0; row<b.length; row++) {
+			int [] arr= b [row];
+			for (int col=0; col<arr.length;col++) {
+				int num= b[row][col];
+				if (num == 0) { 
+					break;
+				} else {
+					zeros--;
+				}
+			}
+		}
+
+		if (zeros == 0) {
+			return false;
+			//return -1;
+		}
+
+		// no win code
+		//return 0;// if there are no winning patterns found, return 0
+		return false;
+	}
+
+}
