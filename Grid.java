2cf396d99ffcc978f956dd516eb70cfbd421fe1b
elisabeth-the-llama <39343552+elisabeth-the-llama@users.noreply.github.com>
2018-06-02 12:43:59 -0700

    Add files via upload

--- Grid.java
+++ Grid.java
@@ -8,13 +8,11 @@ public class Grid {
 		this.board = board;
 	}
 
-	public boolean win(int [][] b,int player) { //code commented out can be put back in if the code needs to be modified to return
-		//who won
+	public boolean win(int [][] b,int player) { 
 		// horizontal/vertical win checking code
 
-		// checks if there is a winning horizontal or vertical pattern and returns who
-		// won (1 or 2), or if it was a
-		// draw (returns -1) or if there is no win or no draw (0)
+		// checks if there is a winning horizontal or vertical pattern and returns if the player won, or if there
+		// were no winning patterns
 		int horiz;
 		for (int row = 0; row < b.length; row++) {
 			horiz = 0; // resets to 0 for each new row
@@ -26,14 +24,9 @@ public class Grid {
 					if (b[row][0] == player) {
 						return true;
 					}
-
-					/*else {
-						return 2;
-					}
-					*/
 					// if the first number(which represents either an x or o, matches
 					// with the rest of the numbers in the row (aka if the entire row has
-					// the same number), return the number which won using horizontal pattern
+					// the same number), return true
 				}
 
 				if (row == 0) {// goes through this only with the first row
@@ -42,12 +35,8 @@ public class Grid {
 						if (b[0][col] == player) {
 							return true;
 						} 
-						/*else {
-							return 2;
-						}
-						*/
 						// if a number in the first row is the same as the two below it,
-						// return the number/ x or o that won with a vertical pattern
+						// return that the player won
 					}
 				}
 			}
@@ -64,44 +53,14 @@ public class Grid {
 			if (mid == player) {
 				return true;
 			} 
-			/*else {
-				return 2;
-			}
-			*/
 		} else if (mid == b[0][2] && mid == b[2][0] && mid != 0) { // top right and bottom left
 			if (mid == player) {
 				return true;
 			} 
-			/*else {
-				return 2;
-			}
-			*/
-		}
-
-		// draw checking code
-
-		// checks if there are no remaining spaces but no win (a draw) and returns -1 if
-		// so
-		int zeros = 9;
-		for (int row= 0; row<b.length; row++) {
-			int [] arr= b [row];
-			for (int col=0; col<arr.length;col++) {
-				int num= b[row][col];
-				if (num == 0) { 
-					break;
-				} else {
-					zeros--;
-				}
-			}
-		}
-
-		if (zeros == 0) {
-			return false;
-			//return -1;
 		}
 
 		// no win code
-		//return 0;// if there are no winning patterns found, return 0
+		// if there are no winning patterns found, return false
 		return false;
 	}
 
